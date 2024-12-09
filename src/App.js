2a5e1b56abbe2cfaad2c7e71449192b2fc57fd4d
NithennAnkaathi <ankaathinithenn@gmail.com>
2024-12-09 10:56:17 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './header/Navbar';
 import './App.css';
-import { Routes, Route } from 'react-router-dom'
+import { Router as Router, Route, Routes } from "react-router";
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/dashboard/Home';
 import Contact from './pages/Contact';
 import MarketOverview from './pages/dashboard/MarketOverview';
@@ -9,19 +11,24 @@ import Wallet from './pages/wallet/Wallet';
 import User from './pages/user/User';
 import Profile from './pages/profile/Profile';
 import Footer from './header/Footer';
-import Subscriptionadminhome from './pages/admin/batch/Subscriptionadminhome';
+// import Subscriptionadminhome from './pages/admin/batch/Subscriptionadminhome';
 import SignUp from './pages/user/signup';
+// import SignUp from './pages/user/signup';
 import Batch from './pages/admin/batch/Batch';
+import SubscriptionRC from "./pages/admin/batch/Components/Subscriptions/SubscriptionRC";
+
 
 function App() {
+  
   return (
-    <div className="App bg-rose-400	  w-full h-screen" >
+    <div className="App bg-rose-400	w-full h-screen" >
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/markets' element={<MarketOverview />} />
         <Route path='/subscription' element={<Subscription />} />
-        <Route path='/Subscriptionadminhome' element={<Subscriptionadminhome />} />
+        {/* <Route path='/Subscriptionadminhome' element={<Subscriptionadminhome />} /> */}
+        <Route path="/subscriptions/*" element={<SubscriptionRC/>} />
         <Route path='/wallet' element={<Wallet />} />
         <Route path='/user' element={<User />} />
         <Route path='/profile' element={<Profile />} />
@@ -30,6 +37,7 @@ function App() {
         <Route path="/batch" element={<Batch />} />
       </Routes>
       <Footer />
+
     </div>
   );
 }
